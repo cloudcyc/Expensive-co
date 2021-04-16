d3c07a6c6a5c745fa37bc6b911025eab45275d19
kahchun0503 <79494250+kahchun0503@users.noreply.github.com>
2021-04-16 12:37:09 +0800

    shops

---
+++ Expensive-co/Development/Shops.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Development/Layout.Master" AutoEventWireup="true" CodeBehind="Shops.aspx.cs" Inherits="Expensive_co.Development.Shops" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+
+
+
+    <form id="form1" runat="server">
+            <div class="row">
+                    <div class="col-md-4">
+                        <div class="card mb-4 product-wap rounded-0">
+                            <div class="card rounded-0">
+                                <div class="card-body">
+
+            <asp:PlaceHolder ID="PlaceHolder1" runat="server"></asp:PlaceHolder>
+
+                                    </div>
+                                </div>
+                             </div>
+                        </div>
+                    </div>
+
+    </form>
+    <br />
+
+
+</asp:Content>
---
+++ Expensive-co/Development/Shops.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace Expensive_co.Development
+{
+    public partial class Shops : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                DataTable dt = this.GetData();
+                StringBuilder html = new StringBuilder();
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    
+
+                    html.Append("<h3>" + row["productName"] + "</h3>");
+                    html.Append(row["productDescription"] + "<br>");
+                    html.Append(row["productPrice"]); 
+
+                    
+                }
+
+                PlaceHolder1.Controls.Add(new Literal { Text = html.ToString() });
+            }
+        }
+        private DataTable GetData()
+        {
+            SqlConnection connect = new SqlConnection(ConfigurationManager.ConnectionStrings["ExpensiveDBConnectionString"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Products", connect);
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter())
+                {
+                    cmd.Connection = connect;
+                    sda.SelectCommand = cmd;
+                    using (DataTable dt = new DataTable())
+                    {
+                        sda.Fill(dt);
+                        return dt;
+                    }
+                }
+            }
+        }
+    }
+}
